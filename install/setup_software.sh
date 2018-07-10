2359f0e81606a7be4f5ba2281168671ec8012f2b
Thomas Eitler <thomas.eitler@gmail.com>
2018-07-10 21:54:29 +0200

    add i3 installation

--- install/setup_software.sh
+++ install/setup_software.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # install system software
-apt install laptop-mode-tools powertop
+apt install -y laptop-mode-tools powertop
 
 # install some programs
 apt install -y curl git neovim redshift redshift-gtk
+
+# install i3
+apt install -y i3
 
 # update firefox
 # wait for firefox 60esr
