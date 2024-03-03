6f5e0a0eb874a1f79cc7e1fd7f8fd9644e7ef6b2
Thomas Schuiki <thomas.schuiki@protonmail.com>
2024-03-03 21:01:34 +0100

    fish: update pure plugin

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,17 +7,12 @@ end
 ###############################################
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_STATE_HOME $HOME/.local/state
 set -x XDG_CACHE_HOME $HOME/.cache
 
 ###############################################
 # Setup PATH
 ###############################################
-## setup go path
-set -x GOPATH $HOME/go
-set -x GOBIN $HOME/go/bin
-set -x GOROOT
-
-set -x PATH $PATH $GOPATH $GOBIN
 ## add local/bin to path
 set -x PATH $PATH $HOME/.local/bin
 
@@ -31,10 +26,41 @@ set -Ux BROWSER firefox
 ###############################################
 # Setup application paths/variables
 ###############################################
-# weechat
+## setup go path
+set -x GOPATH $XDG_DATA_HOME/go
+set -x GOBIN $XDG_DATA_HOME/go/bin
+set -x GOROOT
+
+set -x PATH $PATH $GOPATH $GOBIN
+## android
+set -x ANDROID_HOME $XDG_DATA_HOME/android
+## gpg
+set -x GPG_TTY $(tty)
+set -x GNUPGHOME $XDG_DATA_HOME/gnupg
+## bash history
+set -x HISTFILE $XDG_STATE_HOME/bash/history
+## rustup
+set -x RUSTUP_HOME $XDG_DATA_HOME/rustup
+## cargo
+set -x CARGO_HOME $XDG_DATA_HOME/cargo
+## weechat home
 set -x WEECHAT_HOME $XDG_CONFIG_HOME/weechat
-# gpg
-set -x GPG_TTY (tty)
+## gtk 2
+set -x GTK2_RC_FILES $XDG_CONFIG_HOME/gtk-2.0/gtkrc
+## less
+set -x LESSHISTFILE $XDG_CACHE_HOME/less/history
+## minikube
+set -x MINIKUBE_HOME $XDG_DATA_HOME/minikube
+## java
+set -x _JAVA_OPTIONS -Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java
+## python
+set -x PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
+## inputrc
+set -x INPUTRC $XDG_CONFIG_HOME/readline/inputrc
+## sqlite
+set -x SQLITE_HISTORY $XDG_CACHE_HOME/sqlite_history
+## xinit
+set -x XINITRC $XDG_CONFIG_HOME/X11/xinitrc
 # zoxide
 zoxide init fish | source
 
